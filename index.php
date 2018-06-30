7e367cf2667c36e2f08a3ab2a4069b1fa6e9ce8c
Burak GULDOGAN <40712499+lastburak@users.noreply.github.com>
2018-06-30 14:02:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,278 @@
+
+<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+<head>
+
+
+    <meta charset="utf-8">
+    <title>Burak GULDOGAN | Portflöy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+    <link rel="shortcut icon" href="/favicon.ico?v=2">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css">
+    <link rel="stylesheet" href="/css/flexslider.css">
+    <link id="main" rel="stylesheet" href="/css/designr-theme-cyan2.css">
+    <link id="theme" rel="stylesheet" href="/css/designr-theme-cyan2.css">
+    <link rel="stylesheet" href="/css/style-switcher.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,300,300italic,400italic,700,700italic,900' rel='stylesheet' type='text/css'>
+
+
+    <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+    <script type="text/javascript" src="/js/libs/modernizr.min.js"></script>
+
+
+</head>
+
+<body data-spy="scroll" data-target="#main-nav" data-offset="200">
+
+<div id="main-nav" class="navbar navbar-fixed-top">
+    <div class="container">
+
+        <div class="navbar-header">
+
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
+                <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+            </button>
+
+
+            <a class="navbar-brand scrollto" href="#home">Burak GULDOGAN<br><span>Webmaster</span></a>
+
+
+        </div>
+
+        <div id="site-nav" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="sr-only">
+                    <a href="#home" class="scrollto">Home</a>
+                </li>
+                <li>
+                    <a href="#services" class="scrollto">What I do</a>
+                </li>
+                <li>
+                    <a href="#about" class="scrollto">About Me</a>
+                </li>
+                <li>
+                    <a href="#contact" class="scrollto">Contact Me</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</div>
+
+
+<header id="home" class="jumbotron">
+
+    <div class="container">
+
+        <div class="message-box clearfix">
+
+            <div class="left-col">
+                <p>Ben <span>Burak GULDOGAN</span><br/>Ben web</p>
+            </div>
+
+            <div class="right-col flexslider home-slider" style="z-index:1;">
+
+                <ul class="slides">
+
+                    <li><p>Geliştiriciyim</p></li>
+
+                    <li><p>Tasarımcıyım</p></li>
+
+                    <li><p>Masterim</p></li>
+
+                </ul>
+
+            </div><!--End home-slider -->
+
+        </div><!--End message-box -->
+
+    </div><!--End container -->
+
+</header><!--End header -->
+
+<!-- ==============================================
+SERVICES
+=============================================== -->
+<section id="services">
+
+    <div class="container">
+
+        <h1 class="section-title scrollimation scale-in">The <span>Services</span> I offer<i class="icon-wrench"></i></h1>
+
+        <div class="row services scrollimation fade-left">
+            <div class="col-md-3 text-center">
+                <a class="icon"><i class="icon-flag"></i></a>
+                <h2>SEO</h2>
+            </div>
+            <div class="col-md-3 details">
+                <p>I've got the skills to get your local business above the competition on the largest search engines and make you more visible to your customers. </p>
+            </div>
+            <div class="col-md-3 text-center">
+                <a class="icon"><i class="icon-code"></i></a>
+                <h2>Development</h2>
+            </div>
+            <div class="col-md-3 details">
+                <p>I'm proficient in PhP and JavaScript. I have experience with content management systems such as Drupal. </p>
+            </div>
+        </div><!--End row (top) -->
+
+        <div class="row line visible-md visible-lg">
+            <div class="col-md-3"><span class="spot"></span></div>
+            <div class="col-md-3"><span class="spot"></span></div>
+            <div class="col-md-3"><span class="spot"></span></div>
+            <div class="col-md-3"><span class="spot"></span></div>
+        </div><!--End row -->
+
+        <div class="row services bottom-row scrollimation fade-right">
+            <div class="col-md-3 text-center col-md-push-3">
+                <h2>Design</h2>
+                <a class="icon"><i class="icon-pencil"></i></a>
+            </div>
+            <div class="col-md-3 details col-md-pull-3">
+                <p>I'm skilled in clean, modern design. Crisp and fluid design means customers will spend more time viewing your content. </p>
+            </div>
+            <div class="col-md-3 text-center  col-md-push-3">
+                <h2>Debian Servers</h2>
+                <a class="icon"><i class="icon-terminal"></i></a>
+            </div>
+            <div class="col-md-3 details  col-md-pull-3">
+                <p>I own many servers, one of which this website and many others are hosted on. I also specialize in gaming servers for Minecraft, Teamspeak, and more.</p>
+            </div>
+        </div><!--End row (bottom)-->
+
+    </div><!--End container -->
+
+</section><!--End services section-->
+
+<!-- ==============================================
+ABOUT
+=============================================== -->
+<section id="about">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-1 scrollimation fade-right">
+                <img class="img-responsive img-circle img-center" src="https://pbs.twimg.com/profile_images/705102341960294405/nD4kR7WN_400x400.jpg" alt="">
+            </div>
+            <div class="col-sm-6 col-sm-offset-1 scrollimation fade-left">
+                <h1>Merhaba, Ben Burak GÜLDOĞAN...</h1>
+                <p>Ben bir Webmasterim, Captial-City(Sofia)'de yaşıyorum. Kendi sunucularım mevcut hosting işleri ile uğraşmaktayım . </p>
+
+            </div>
+        </div><!--End row -->
+
+    </div><!--End container -->
+
+</section><!--End about section -->
+
+<!-- ==============================================
+SKILLS
+=============================================== -->
+<section id="skills">
+
+    <div class="container">
+
+        <div class="row skills">
+
+            <div class="col-sm-6 col-md-3 text-center">
+                <span class="chart" data-percent="90"><span class="percent">64</span></span>
+                <h2 class="text-center">HTML5</h2>
+            </div>
+            <div class="col-sm-6 col-md-3 text-center">
+                <span class="chart" data-percent="65"><span class="percent">81</span></span>
+                <h2 class="text-center">PhP</h2>
+            </div>
+            <div class="col-sm-6 col-md-3 text-center">
+                <span class="chart" data-percent="99"><span class="percent">100</span></span>
+                <h2 class="text-center">Sunucu Yönetimi</h2>
+            </div>
+            <div class="col-sm-6 col-md-3 text-center">
+                <span class="chart" data-percent="85"><span class="percent">77</span></span>
+                <h2 class="text-center">İşletim Sistemi Yönetimi</h2>
+            </div>
+
+        </div><!--End row -->
+
+    </div><!--End container -->
+
+</section><!--End skills section -->
+
+<!-- ==============================================
+PORTFOLIO
+=============================================== -->
+
+
+<!-- ==============================================
+CONTACT
+=============================================== -->
+<section  id="contact">
+
+    <div class="container">
+
+        <h1 class="section-title scrollimation scale-in">Keep in <span>Touch</span> with me</h1>
+
+        <div class="row">
+
+            <div class="typeform-widget" data-url="https://burakgldoan.typeform.com/to/JFeiKl" style="width: 100%; height: 500px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by <a href="https://admin.typeform.com/signup?utm_campaign=JFeiKl&utm_source=typeform.com-12272299-Basic&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN" style="color: #999" target="_blank">Typeform</a> </div>
+
+        </div><!-- End row -->
+
+    </div><!-- End container -->
+
+</section><!-- End contact section -->
+
+<!-- ==============================================
+FOOTER
+=============================================== -->
+<footer id="main-footer">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-sm-6 left-col">
+                <h1 class="small-logo">BURAK GULDOGAN<br><span>WEBMASTER</span></h1>
+                <ul class="footer-nav">
+                    <li><a class="scrollto" href="#home">ANASAYFA</a></li>
+                    <li><a class="scrollto" href="#services">Hizmetler</a></li>
+                    <li><a class="scrollto" href="#contact">İletişim</a></li>
+                </ul>
+                <p>BurakGULDOGAN 2018 &copy; Tüm Hakları Saklıdır.</p>
+            </div>
+
+            <div class="col-sm-6 right-col">
+                <h2>Ben Kimmiyim?</h2>
+                <p>Ben bir öğrenciyim, Bilgisayar - Bilişim işlerine gönül vermiş bilgisayar bilişim okuyan bir öğrenciyim. İşletim sistemleri ve sunucular hobilerim, Yazılım dilleri fobilerim, yazmak için üşengeç olmamam gerekiyor ama öyleyim ne yazıkki...</p>
+            </div>
+
+        </div><!-- End row -->
+
+    </div><!-- End container -->
+
+</footer><!-- End footer -->
+
+<!-- ==============================================
+Le Java Script
+=============================================== -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src='/js/jquery.scrollto.js'></script>
+<script src='/js/jquery.flexslider.min.js'></script>
+<script src='/js/twitterFetcher_v10_min.js'></script>
+<script src='/js/jquery.masonry.min.js'></script>
+<script src="/js/waypoints.min.js"></script>
+<script src="/js/jquery.easypiechart.js"></script>
+<script src="/js/jquery.backstretch.min.js"></script>
+<script src="/js/designr.js"></script>
+<script src="/js/style-switcher.js"></script>
+
+</body>
+</html>
